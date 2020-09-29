c6139aedda258f1873394e455cb2afddd41e34ba
FMAlfian <fm.alfian@gmail.com>
2020-09-29 13:16:01 +0700

    autocomplete plugin process list post

--- autocomplete.js
+++ autocomplete.js
@@ -2,22 +2,6 @@ jQuery(document).ready(function($) {
 	
 	// AJAX url
 	var ajax_url = autocomplete_ajax.ajax_url;	
-  
-	// $( "#search" ).keyup(function(){
-	// 	$.ajax({
-	// 		url: ajax_url,
-	// 		type: 'post',
-	// 		data: {
-	// 			'action': 'autocomplete_post',
-	// 			'search': $(this).val(),
-	// 		},
-	// 		dataType: 'json',
-	// 		success: function(data){
-				
-	// 			console.log(data);
-	// 		}
-	// 	});
-	// });
 
 	$( "#search" ).autocomplete({
 		source: function( request, response ) {
@@ -33,7 +17,27 @@ jQuery(document).ready(function($) {
 					response( data );
 				}
 			});
-		  },
+		},
+	});
+
+	$( "#submit" ).click(function(){		
+		$.ajax({
+			url: ajax_url,
+			type: 'post',
+			data: {
+				'action': 'list_post',
+				'submit': $('#search').val(),
+			},
+			dataType: 'json',
+			success: function(data){	
+				var html = '';
+				$.each(data, function(index, item){
+					html += '<div><h2>'+item+'</h2></div>';
+				});				
+				
+				$('#result').html(html);
+			}
+		});
 	});
 
 });
--- autocomplete.php
+++ autocomplete.php
@@ -13,10 +13,12 @@ class Autocomplete {
 
     public function wp6_training_sc() {
         $widget =  '<div>
-                        <form action="#fsearch" id="fsearch">
+                        <form id="fsearch">
                             <input type="text" name="search" id="search">
-                            <input type="submit" name="submit" value="Search">
+                            <input type="button" id="submit" value="Search">
                         </form>
+                    </div>
+                    <div id="result">
                     </div>';
         
         return $widget;
@@ -49,6 +51,23 @@ class Autocomplete {
         wp_die();
     }
 
+    public function list_post_callback() {
+        global $wpdb;
+
+        $post = $_POST['submit'];
+        $response = array();
+
+        $query = new WP_Query( array( 'post_type' => 'post', 's' =>  $post, 'post_status' => 'publish') );        
+        while ( $query->have_posts() ) {
+            $query->the_post();
+            array_push($response, get_the_title());
+        }
+        wp_reset_postdata();
+
+        echo json_encode($response);
+        wp_die();
+    }
+
 }
 
 $autocomplete = new Autocomplete();
@@ -57,5 +76,8 @@ add_action('wp_enqueue_scripts', array($autocomplete, 'enqueue_autocomplete'));
 
 add_action( 'wp_ajax_autocomplete_post', array($autocomplete,'autocomplete_callback') );
 add_action( 'wp_ajax_nopriv_autocomplete_post', array($autocomplete,'autocomplete_callback') );
+
+add_action( 'wp_ajax_list_post', array($autocomplete,'list_post_callback') );
+add_action( 'wp_ajax_nopriv_list_post', array($autocomplete,'list_post_callback') );
 
 add_shortcode('wp6_training', array($autocomplete, 'wp6_training_sc'));
